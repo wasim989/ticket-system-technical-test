9cf67aafe546659d475501812e1a9fd42e086186
= <=>
2025-07-16 17:31:31 +0100

    stage 2 unit tests

--- test/data/TicketServiceData.js
+++ test/data/TicketServiceData.js
@@ -9,8 +9,23 @@ let generateTicketRequests = (ticketOrders) => {
 }
 
 export default {
+    
     negativeOneAdults: generateTicketRequests([{type: 'ADULT', quantity: -1}]),
     zeroAdults: generateTicketRequests([{type: 'ADULT', quantity: 0}]),
     oneAdult: generateTicketRequests([{type: 'ADULT', quantity: 1}]),
+
+    allTypesMaxTotalQuantity: generateTicketRequests([{type: 'ADULT', quantity: 9}, {type: 'CHILD', quantity: 8}, {type: 'INFANT', quantity: 8}]),
     allTypesMaxMinusOneTotalQuantity: generateTicketRequests([{type: 'ADULT', quantity: 8}, {type: 'CHILD', quantity: 8}, {type: 'INFANT', quantity: 8}]),
+    allTypesMaxPlusOneTotalQuantity: generateTicketRequests([{type: 'ADULT', quantity: 9}, {type: 'CHILD', quantity: 9}, {type: 'INFANT', quantity: 8}]),
+
+    oneChild: generateTicketRequests([ {type: 'CHILD', quantity: 1}]),
+    oneAdultOneChild: generateTicketRequests([{type: 'ADULT', quantity: 1}, {type: 'CHILD', quantity: 1}]),
+    maxAdultOneChild: generateTicketRequests([{type: 'ADULT', quantity: 24}, {type: 'CHILD', quantity: 1}]),
+    oneAdultMaxChild: generateTicketRequests([{type: 'ADULT', quantity: 1}, {type: 'CHILD', quantity: 24}]),
+
+    oneInfant: generateTicketRequests([{type: 'INFANT', quantity: 1}]),
+    oneAdultOneInfant: generateTicketRequests([{type: 'ADULT', quantity: 1}, {type: 'INFANT', quantity: 1}]),
+    oneAdultTwoInfants: generateTicketRequests([{type: 'ADULT', quantity: 1}, {type: 'INFANT', quantity: 2}]),
+    twoAdultOneInfant: generateTicketRequests([{type: 'ADULT', quantity: 2}, {type: 'INFANT', quantity: 1}]),
+
 };
--- test/unit/TicketService.test.js
+++ test/unit/TicketService.test.js
@@ -19,13 +19,41 @@ beforeEach(() => {
 
 describe('request validation tests', () => {
 
+  test('purchaseTickets method should throw InvalidPurchaseException if purchase not of type TicketTypeRequest', () => {
+    expect(() => {ticketService.purchaseTickets(1, [])}).toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, null)}).toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, "123")}).toThrow(InvalidPurchaseException);
+  });
+  
   test('purchaseTickets method should throw InvalidPurchaseException if account Id is not an integer greater than 0', () => {
-    expect(() => {ticketService.purchaseTickets('123', ...TicketServiceData.allTypesMaxMinusOneTotalQuantity)}).toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets('123', ...TicketServiceData.allTypesMaxTotalQuantity)}).toThrow(InvalidPurchaseException);
     expect(() => {ticketService.purchaseTickets(null, ...TicketServiceData.oneAdult)}).toThrow(InvalidPurchaseException);
     expect(() => {ticketService.purchaseTickets({}, ...TicketServiceData.oneAdult)}).toThrow(InvalidPurchaseException);
     expect(() => {ticketService.purchaseTickets(0, ...TicketServiceData.oneAdult)}).toThrow(InvalidPurchaseException);
     expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.oneAdult)}).not.toThrow(InvalidPurchaseException);
     expect(() => {ticketService.purchaseTickets(2, ...TicketServiceData.oneAdult)}).not.toThrow(InvalidPurchaseException);
+  });
+
+  test('purchaseTickets method should throw InvalidPurchaseException if ticket quantity is not an integer value between 0 and 26', () => {
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.negativeOneAdults)}).toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.zeroAdults)}).toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.allTypesMaxTotalQuantity)}).not.toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.oneAdult)}).not.toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.allTypesMaxMinusOneTotalQuantity)}).not.toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.allTypesMaxPlusOneTotalQuantity)}).toThrow(InvalidPurchaseException);
+  });
+
+  test('purchaseTickets method should throw InvalidPurchaseException if child tickets are being purchased without at least 1 adult ticket', () => {
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.oneChild)}).toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.oneAdultOneChild)}).not.toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.maxAdultOneChild)}).not.toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.oneAdultMaxChild)}).not.toThrow(InvalidPurchaseException);
+  }); 
+
+  test('purchaseTickets method should throw InvalidPurchaseException if there is not an adult ticket being purchased for every infant ticket', () => {
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.twoAdultOneInfant)}).not.toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.oneAdultOneInfant)}).not.toThrow(InvalidPurchaseException);
+    expect(() => {ticketService.purchaseTickets(1, ...TicketServiceData.oneAdultTwoInfants)}).toThrow(InvalidPurchaseException);
   });
 
 });
